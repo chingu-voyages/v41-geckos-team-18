c748d706bfab67cd8d751cb5ef7e8de18dc4288c
Chinh Do <do.inby@gmail.com>
2022-10-14 17:58:33 +1100

    Set up Netlify CLI. Add fetch API test

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,19 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react';
 
 function App() {
+  useEffect(() => {
+    fetch("/.netlify/functions/fetch-movie?name='Bob'").then((res) =>
+      console.log(res.json())
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
