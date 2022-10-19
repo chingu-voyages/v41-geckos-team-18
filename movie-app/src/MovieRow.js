6db08c75885f593f84c043b7a3bb48581ec2182c
sukyungdev <sukyung.developer@gmail.com>
2022-10-19 23:23:59 +0900

    Add : style-components

--- movie-app/src/MovieRow.js
+++ movie-app/src/MovieRow.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import './MovieRow.css';
 
 const MovieRow = ({ title, id, data }) => {
   const [movie, setMovie] = useState([]);
@@ -14,22 +16,40 @@ const MovieRow = ({ title, id, data }) => {
   return (
     <section className="row">
       <h2>{title}</h2>
-      <div id={id} className="row-poster">
+      <PosterContainer id={id}>
         {movie &&
           movie.map((movie) => (
-            <div>
+            <PosterBox>
               {/* <p>{movie.name}</p> */}
-              <img
+              <PosterImg
                 key={movie.id}
                 className="poster"
                 src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
                 alt={movie.name}
               />
-            </div>
+            </PosterBox>
           ))}
-      </div>
+      </PosterContainer>
     </section>
   );
 };
+
+const PosterContainer = styled.div`
+  display: flex;
+  overflow-y: hidden;
+  overflow-x: scroll;
+  scroll-behavior: smooth;
+`;
+
+const PosterBox = styled.div`
+  margin-right: 10px;
+`;
+
+const PosterImg = styled.img`
+  min-width: 200px;
+  width: 100%;
+  border-radius: 8px;
+  height: auto;
+`;
 
 export default MovieRow;
