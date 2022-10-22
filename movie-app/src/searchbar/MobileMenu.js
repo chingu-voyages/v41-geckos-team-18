7f9f560c24fe754013bc199468f750c20c759d6f
Miho Funayama <mihofunayama@QB-JP-110.local>
2022-10-22 15:23:42 +0900

    Transferred corresponding elements from SearchBar.js component

--- movie-app/src/searchbar/MobileMenu.js
+++ movie-app/src/searchbar/MobileMenu.js
@@ -1,46 +1,57 @@
+import { useState } from 'react';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 
-export default function Menu () {
-    const mobileMenuId = 'primary-search-account-menu-mobile';
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <Link to="/movies"><p color="inherit">Movies</p></Link>
-      </MenuItem>
-      <MenuItem>
-        <Link to="/tvShows"><p color="inherit">TV Shows</p></Link>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <Icon
-          size="large"
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <AccountCircle />
-        </Icon>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-      );
+export default function MobileMenu() {
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState('');
 
-      return (
-        <Menu />
-      )
-        }
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMobileMenuOpen = (e) => {
+    setMobileMoreAnchorEl(e);
+  };
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+
+  return  (<Menu
+  anchorEl={mobileMoreAnchorEl}
+  anchorOrigin={{
+    vertical: 'top',
+    horizontal: 'right',
+  }}
+  id={mobileMenuId}
+  keepMounted
+  transformOrigin={{
+    vertical: 'top',
+    horizontal: 'right',
+  }}
+  open={isMobileMenuOpen}
+  onClose={handleMobileMenuClose}
+>
+  <MenuItem>
+    <Link to="/movies">
+      <p color="inherit">Movies</p>
+    </Link>
+  </MenuItem>
+  <MenuItem>
+    <Link to="/tvShows">
+      <p color="inherit">TV Shows</p>
+    </Link>
+  </MenuItem>
+  <MenuItem onClick={handleProfileMenuOpen}>
+    <Icon
+      size="large"
+      aria-label="account of current user"
+      aria-controls="primary-search-account-menu"
+      aria-haspopup="true"
+      color="inherit"
+    >
+      <AccountCircle />
+    </Icon>
+    <p>Profile</p>
+  </MenuItem>
+</Menu>);
+}
