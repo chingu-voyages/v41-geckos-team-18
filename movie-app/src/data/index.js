ab2ed9f8a974d53f9242b2e026a13d275dce75a9
Chinh Do <chinh@doinby.co>
2022-10-25 22:08:36 +1100

    Re-format code using prettier-ignore

--- movie-app/src/data/index.js
+++ movie-app/src/data/index.js
@@ -1,45 +1,9 @@
-const movieGenres = {
-  action: 28,
-  adventure: 12,
-  animation: 16,
-  comedy: 35,
-  crime: 80,
-  documentary: 99,
-  drama: 18,
-  family: 10751,
-  fantasy: 14,
-  history: 36,
-  horror: 27,
-  music: 10402,
-  mystery: 9648,
-  romance: 10749,
-  scienceFiction: 878,
-  tvMovie: 10770,
-  thriller: 53,
-  war: 10752,
-  western: 37,
-};
+// prettier-ignore
+const movieGenres = { action: 28, adventure: 12, animation: 16, comedy: 35, crime: 80, documentary: 99, drama: 18, family: 10751, fantasy: 14, history: 36, horror: 27, music: 10402, mystery: 9648, romance: 10749, scienceFiction: 878, tvMovie: 10770, thriller: 53, war: 10752, western: 37 };
 
+// prettier-ignore
 const getGenreMoodPairings = () => {
-  const {
-    action,
-    adventure,
-    animation,
-    comedy,
-    crime,
-    documentary,
-    drama,
-    family,
-    fantasy,
-    history,
-    horror,
-    music,
-    mystery,
-    romance,
-    scienceFiction,
-    thriller,
-    war,
-  } = movieGenres;
+  const { action, adventure, animation, comedy, crime, documentary, drama, family, fantasy, history, horror, music, mystery, romance, scienceFiction, thriller, war } = movieGenres;
   return {
     funny: `${comedy},${adventure}`,
     romantic: `${romance}`,
