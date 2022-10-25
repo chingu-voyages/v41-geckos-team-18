d92db8805a5cc7186032bd5765be85aa7c577422
Chinh Do <chinh@doinby.co>
2022-10-25 22:27:21 +1100

    Add example mood buttons

---
+++ movie-app/src/components/MoodButtons.js
@@ -0,0 +1,66 @@
+import { Grid, Button } from '@mui/material';
+import movieMoods from '../data';
+
+function MoodButtons({ setUrl }) {
+  return (
+    <Grid container direction="row" spacing={2}>
+      <Grid item>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => {
+            setUrl(`/api/fetch-trending-all`);
+          }}
+        >
+          Trending
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => {
+            setUrl(`/api/fetch-movie-mood?moodId=${movieMoods.funny}`);
+          }}
+        >
+          Funny
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => {
+            setUrl(`/api/fetch-movie-mood?moodId=${movieMoods.romantic}`);
+          }}
+        >
+          Romantic
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => {
+            setUrl(`/api/fetch-movie-mood?moodId=${movieMoods.dark}`);
+          }}
+        >
+          Dark
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => {
+            setUrl(`/api/fetch-movie-mood?moodId=${movieMoods.excited}`);
+          }}
+        >
+          Excited
+        </Button>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default MoodButtons;
