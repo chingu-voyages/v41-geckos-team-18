0fef907baf16f3493946cd7f2adcf322df288c4b
Chinh Do <chinh@doinby.co>
2022-10-27 21:13:43 +1100

    Re-apply missing code from fetch-movie-mood function

--- movie-app/functions/fetch-movie-mood/fetch-movie-mood.js
+++ movie-app/functions/fetch-movie-mood/fetch-movie-mood.js
@@ -1,17 +1,17 @@
 const axios = require('axios');
 
 const handler = async (event) => {
-  const { moodId } = event.queryStringParameters;
+  const { moodId, pageId } = event.queryStringParameters;
   const API_KEY = `api_key=${process.env.API_KEY}`;
   const BASE_URL = 'https://api.themoviedb.org/3';
-  const url = `${BASE_URL}/discover/movie?${API_KEY}&language=en-US&include_adult=false&with_genres=${moodId}`;
+  const url = `${BASE_URL}/discover/movie?${API_KEY}&language=en-US&include_adult=false&with_genres=${moodId}&page=${pageId}`;
 
   try {
     const { data } = await axios.get(url);
 
     return {
       statusCode: 200,
-      body: JSON.stringify(data),
+      body: JSON.stringify(data, null, 2),
     };
   } catch (err) {
     console.log(err);
