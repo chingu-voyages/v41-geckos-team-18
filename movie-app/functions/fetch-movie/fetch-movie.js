d63622d42135e91cadfeda654f6447eba7fc5f40
Chinh Do <chinh@doinby.co>
2022-10-28 21:30:45 +1100

    Reconfigure Netlify function to fetch multiple endpoints

--- movie-app/functions/fetch-movie/fetch-movie.js
+++ movie-app/functions/fetch-movie/fetch-movie.js
@@ -3,15 +3,35 @@ const axios = require('axios');
 const handler = async (event) => {
   const API_KEY = process.env.API_KEY;
   const BASE_URL = 'https://api.themoviedb.org/3/';
+  // const { movieId } = event.queryStringParameter;
   const imdbID = 'tt0111161';
-  const movieURL = `${BASE_URL}/movie/${imdbID}?api_key=${API_KEY}`;
+
+  const endpoints = [
+    `${BASE_URL}/movie/${imdbID}?api_key=${API_KEY}`,
+    `${BASE_URL}/movie/${imdbID}/videos?api_key=${API_KEY}`,
+    `${BASE_URL}/movie/${imdbID}/keywords?api_key=${API_KEY}`,
+    `${BASE_URL}/movie/${imdbID}/credits?api_key=${API_KEY}`,
+    `${BASE_URL}/movie/${imdbID}/similar?api_key=${API_KEY}`,
+  ];
 
   try {
-    const { data } = await axios.get(movieURL);
+    const res = await axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then(
+      axios.spread(
+        (
+          { data: details },
+          { data: videos },
+          { data: keywords },
+          { data: credits },
+          { data: similar },
+        ) => {
+          return { details, videos, keywords, credits, similar };
+        },
+      ),
+    );
 
     return {
       statusCode: 200,
-      body: JSON.stringify(data, null, 2),
+      body: JSON.stringify(res, null, 2),
     };
   } catch (err) {
     console.log(err);
