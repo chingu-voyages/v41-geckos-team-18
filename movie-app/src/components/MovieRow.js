9614f2725ab49f9f2c8bd2792292d7ec56e166c8
Chinh Do <chinh@doinby.co>
2022-10-29 20:09:05 +1100

    FIx no-unused-vars error

--- movie-app/src/components/MovieRow.js
+++ movie-app/src/components/MovieRow.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useState } from 'react';
-import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
