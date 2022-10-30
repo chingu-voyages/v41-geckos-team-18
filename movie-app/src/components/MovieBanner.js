7d220f6638341f6c9407deb7b364221e91043d7e
sukyungdev <sukyung.developer@gmail.com>
2022-10-31 00:14:21 +0900

    add : movie banner content

--- movie-app/src/components/MovieBanner.js
+++ movie-app/src/components/MovieBanner.js
@@ -1,12 +1,13 @@
+/* eslint-disable no-undef */
 import React, { useState } from 'react';
 import Carousel from 'react-material-ui-carousel';
 import useFetch from '../hooks/useFetch';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
 
 const MovieBanner = () => {
-  // const [url, setUrl] = useState(`/api/fetch-trending-all`);
   const [url] = useState(`/api/fetch-trending-all`);
   const {
     data: { results: movie },
@@ -15,9 +16,9 @@ const MovieBanner = () => {
   } = useFetch(url);
 
   const item = movie && movie.slice(0, 5);
-  console.log('isLoading', item);
+
   return (
-    <Container fixed sx={{}}>
+    <Container>
       {isLoading ? (
         'Loading...'
       ) : (
@@ -26,6 +27,24 @@ const MovieBanner = () => {
             <Carousel navButtonsAlwaysVisible={true} duration={1000}>
               {item.map((movie) => (
                 <Box key={movie.id} sx={{ width: '100%', maxHeight: '500px' }}>
+                  {/* {setRatingValue(movie.vote_average)} */}
+                  <Typography sx={{ position: 'absolute', top: '30px' }}>
+                    Movie of the Day
+                  </Typography>
+                  <Typography
+                    sx={{ position: 'absolute', top: '50px' }}
+                    variant="h3"
+                    component="h1"
+                  >
+                    {movie.title ? movie.title : movie.name}
+                  </Typography>
+                  <Rating
+                    name="half-rating-read"
+                    defaultValue={(movie.vote_average / 2).toFixed(2)}
+                    precision={0.5}
+                    readOnly
+                  />
+
                   <img
                     className="poster"
                     src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
