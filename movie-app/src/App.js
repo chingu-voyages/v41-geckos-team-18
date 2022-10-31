105813ae19e4e607b75d178fd7cd62d067624425
Sukyung Kim <96860670+sukyungdev@users.noreply.github.com>
2022-10-31 11:02:16 +0900

    add : import MovieRow

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,6 +1,7 @@
 import SearchBar from './components/searchbar/SearchBar';
 import './App.css';
 import MovieBanner from './components/MovieBanner';
+import MovieRow from './components/MovieRow';
 import Main from './page/Main';
 
 function App() {
