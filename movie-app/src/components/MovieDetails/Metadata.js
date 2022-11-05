63bbe8eb5b7176bce6049e7d83c3494b8d25a97b
Chinh Do <chinh@doinby.co>
2022-11-05 18:55:44 +1100

    Remove unused code

--- movie-app/src/components/MovieDetails/Metadata.js
+++ movie-app/src/components/MovieDetails/Metadata.js
@@ -1,31 +1,16 @@
-import { Container, Grid, Typography } from '@mui/material';
-import { BASE_IMG_URL, getReleaseYear, getRuntime } from '../../data';
-import { getGenres, getKeywords } from '../../data';
-import StarRating from '../StarRating';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { Grid, Typography } from '@mui/material';
+import { getKeywords } from '../../data';
 
 export default function Metadata({ data }) {
   // Destructuring nullable object. Prevent ".map() is not a function" error
   // prettier-ignore
   const {
-    details: {
-      title, popularity, release_date: releaseDate, genres, overview, poster_path: posterUrl, vote_average: voteAverage, runtime,
-    } = {},
+    details: { title, overview } = {},
     keywords: { keywords } = {},
   } = data || {};
 
   return (
     <>
-      {/* <Grid container item gap={2}>
-        <Grid item>
-          <StarRating voteAverage={voteAverage} />
-        </Grid>
-        <Grid item>{getRuntime(runtime)}</Grid>
-        <Grid item>{getReleaseYear(releaseDate)}</Grid>
-        <Grid item>
-          <FavoriteBorderIcon />
-        </Grid>
-      </Grid> */}
       <Grid container item gap={2}>
         <Grid item>
           <Typography variant="h2">{title}</Typography>
