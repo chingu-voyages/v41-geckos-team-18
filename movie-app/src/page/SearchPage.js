4616183b4b4a3c114fb2b72e152b2d46b3043582
sukyungdev <sukyung.developer@gmail.com>
2022-11-05 22:33:12 +0900

    render movie poster

--- movie-app/src/page/SearchPage.js
+++ movie-app/src/page/SearchPage.js
@@ -1,7 +1,10 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
-// import { BASE_IMG_URL, isValid } from '../data';
+import { BASE_IMG_URL, isValid } from '../data';
+import MovieModal from '../components/MovieModal';
+// import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 const SearchPage = () => {
   const useQuery = () => {
@@ -12,8 +15,45 @@ const SearchPage = () => {
   const { data, isLoading, error } = useFetch(`/api/fetch-search-movie?searchTerm=${searchTerm}`);
   const { results: movies } = data || {};
 
+  const [selectedMovie, setSelectedMovie] = useState({});
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = (movieData) => {
+    setOpen(true);
+    setSelectedMovie(movieData);
+  };
+  const handleClose = () => setOpen(false);
+
   console.log(movies, isLoading, error);
-  return <div></div>;
+  return (
+    <div>
+      {isLoading ? (
+        'Loading...'
+      ) : (
+        <>
+          <Box>
+            {isValid(movies)
+              ? movies.map((movies) => (
+                  <Box key={movies.id}>
+                    <img
+                      className="poster"
+                      src={`${BASE_IMG_URL}/w500/${movies.poster_path}`}
+                      alt={movies.name}
+                      onClick={() => handleOpen(movies)}
+                      loading="lazy"
+                      style={{
+                        width: '100%',
+                        cursor: 'pointer',
+                      }}
+                    />
+                  </Box>
+                ))
+              : error}
+          </Box>
+          <MovieModal open={open} handleClose={handleClose} selectedMovie={selectedMovie} />
+        </>
+      )}
+    </div>
+  );
 };
 
 export default SearchPage;
