342b4b60b13711734120cc0a5a23c625aa185880
Chinh Do <chinh@doinby.co>
2022-11-05 21:13:17 +1100

    Refactor `MovieGallery.js`

--- movie-app/src/components/MoodGallery/MoodGallery.js
+++ movie-app/src/components/MoodGallery/MoodGallery.js
@@ -1,10 +1,13 @@
 import { useState } from 'react';
-import { Typography } from '@mui/material';
+import { Pagination, Box } from '@mui/material';
+import { movieMoods } from '../../data';
 import MoodTabs from './MoodTabs';
+import MoodContent from './MoodContent';
 
 export default function MoodGallery() {
   const [value, setValue] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   const handleTabChange = (e) => {
     const index = Number(e.target.getAttribute('index'));
@@ -12,16 +15,40 @@ export default function MoodGallery() {
     setCurrentPage(1);
   };
 
+  const handlePageChange = (e, newPage) => setCurrentPage(newPage);
+
+  function TabPanel({ children, value, index, ...other }) {
+    return (
+      <Box role="tabpanel" hidden={value !== index} {...other}>
+        {value === index && <Box>{children}</Box>}
+      </Box>
+    );
+  }
+
   return (
     <>
-      <Typography component="h1" variant="h5">
-        What are you in the mood for?
-      </Typography>
-      <MoodTabs
-        value={value}
-        handleTabChange={handleTabChange}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
+      <MoodTabs value={value} handleTabChange={handleTabChange} />
+
+      {/* Mood Content */}
+      {movieMoods.map((mood, idx) => {
+        return (
+          <TabPanel key={mood.name} value={value} index={idx} minHeight={375}>
+            <MoodContent
+              genre={mood.genre}
+              setTotalPages={setTotalPages}
+              currentPage={currentPage}
+            />
+          </TabPanel>
+        );
+      })}
+
+      <Pagination
+        count={totalPages}
+        onChange={handlePageChange}
+        page={currentPage}
+        defaultPage={1}
+        color="secondary"
+        sx={{ display: 'flex', placeContent: 'center' }}
       />
     </>
   );
