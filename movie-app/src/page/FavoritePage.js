00acfa5557c2e45e64545e7f2cbe3c53345ea856
sukyungdev <sukyung.developer@gmail.com>
2022-11-10 20:33:01 +0900

    favorite poster render

--- movie-app/src/page/FavoritePage.js
+++ movie-app/src/page/FavoritePage.js
@@ -1,21 +1,95 @@
 import React, { useState, useEffect } from 'react';
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import MovieModal from '../components/MovieModal';
 
 const FavoritePage = () => {
   const [movieData, setMovieData] = useState([]);
+
   const getData = () => {
+    let data = [];
     for (let i = 0; i < localStorage.length; i++) {
       let key = localStorage.key(i);
-      setMovieData(...movieData, localStorage.getItem(`${key}`));
-      console.log(key);
+      data.push(JSON.parse(localStorage.getItem(`${key}`)));
     }
+    console.log(data);
+    setMovieData(data);
+  };
+
+  const deleteData = (id) => {
+    console.log(id);
+    localStorage.removeItem(`${id}`);
+    getData();
   };
 
   useEffect(() => {
     getData();
   }, []);
 
-  return <Container></Container>;
+  const [selectedMovie, setSelectedMovie] = useState({});
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = (movieData) => {
+    setOpen(true);
+    setSelectedMovie(movieData);
+  };
+  const handleClose = () => setOpen(false);
+
+  return (
+    <Container>
+      <Typography component="h2" variant="h5" sx={{ textAlign: 'center' }} m={3}>
+        Favorite Movie
+      </Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+          width: '100%',
+          gap: '5px 10px',
+        }}
+      >
+        {movieData.length > 0 ? (
+          movieData.map((movies) => (
+            <Box key={movies.id} sx={{ width: '30%', maxWidth: '250px', position: 'relative' }}>
+              <img
+                className="poster"
+                src={`https://image.tmdb.org/t/p/w500/${movies.poster_path}`}
+                alt={movies.name}
+                onClick={() => handleOpen(movies)}
+                loading="lazy"
+                style={{
+                  width: '100%',
+                  cursor: 'pointer',
+                }}
+              />
+              <IconButton
+                component="label"
+                sx={{ position: 'absolute', bottom: '0', right: '0', zIndex: '999' }}
+                size="large"
+                onClick={() => deleteData(movies.id)}
+              >
+                <DeleteIcon color="primary" />
+              </IconButton>
+            </Box>
+          ))
+        ) : (
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <p>There are no movies!</p>
+          </Box>
+        )}
+      </Box>
+      <MovieModal open={open} handleClose={handleClose} selectedMovie={selectedMovie} />
+    </Container>
+  );
 };
 
 export default FavoritePage;
