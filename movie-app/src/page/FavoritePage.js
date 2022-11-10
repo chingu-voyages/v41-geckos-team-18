749800ce9a778187af92d5eac0312930a4034b77
sukyungdev <sukyung.developer@gmail.com>
2022-11-10 18:56:41 +0900

    add : localStorge movie data get

--- movie-app/src/page/FavoritePage.js
+++ movie-app/src/page/FavoritePage.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Container from '@mui/material/Container';
 
 const FavoritePage = () => {
-  return <div>FavoritePage</div>;
+  const [movieData, setMovieData] = useState([]);
+  const getData = () => {
+    for (let i = 0; i < localStorage.length; i++) {
+      let key = localStorage.key(i);
+      setMovieData(...movieData, localStorage.getItem(`${key}`));
+      console.log(key);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <Container></Container>;
 };
 
 export default FavoritePage;
